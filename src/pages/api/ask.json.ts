0a205ad059c9db598d8e59a520eb82c22d90cd6f
Josh Richards <joshuarichards001@gmail.com>
2025-04-04 14:12:30 +1300

    add OpenAI dependency, and enhance idiom response

--- src/pages/api/ask.json.ts
+++ src/pages/api/ask.json.ts
@@ -1,46 +1,40 @@
 import type { APIRoute } from "astro";
+import { OpenAI } from "openai";
 
 export const POST: APIRoute = async ({ request }) => {
   const openaiApiKey = import.meta.env.OPENAI_API_KEY;
   const openaiSystemPrompt = import.meta.env.OPENAI_SYSTEM_PROMPT;
+  const openaiListOfIdioms = import.meta.env.OPENAI_LIST_OF_IDIOMS;
 
   const body = await request.json();
   const prompt = body.prompt;
 
-  const openaiBody = {
-    model: "gpt-4o-mini",
-    messages: [
-      { role: "system", content: openaiSystemPrompt },
-      { role: "user", content: prompt },
-    ],
-    max_tokens: 100,
-  };
+  const client = new OpenAI({
+    apiKey: openaiApiKey,
+  });
 
-  const openaiRequest = new Request(
-    "https://api.openai.com/v1/chat/completions",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${openaiApiKey}`,
-      },
-      body: JSON.stringify(openaiBody),
-    }
-  );
+  try {
+    const completion = await client.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        { role: "system", content: openaiSystemPrompt },
+        { role: "user", content: openaiListOfIdioms },
+        { role: "user", content: prompt },
+      ],
+      max_tokens: 100,
+    });
 
-  const openaiResponse = await fetch(openaiRequest);
-  if (!openaiResponse.ok) {
+    const message = completion.choices[0].message?.content;
+
+    return new Response(
+      JSON.stringify({
+        response: message,
+      })
+    );
+  } catch (error) {
+    console.error("Error communicating with OpenAI API:", error);
     return new Response("Error fetching response from OpenAI API", {
       status: 500,
     });
   }
-
-  const openaiData = await openaiResponse.json();
-  const openaiMessage = openaiData.choices[0].message.content;
-
-  return new Response(
-    JSON.stringify({
-      response: openaiMessage,
-    })
-  );
 };
