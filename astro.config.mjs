f7642810e61ab9174ac8b58b3736c5b0236df0ef
Josh Richards <joshuarichards001@gmail.com>
2025-04-04 14:14:09 +1300

    Add Cloudflare adapter and update dependencies in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
